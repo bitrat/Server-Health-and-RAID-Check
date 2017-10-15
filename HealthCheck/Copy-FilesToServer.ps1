c709443a8ac2ce93e8c6749e7d0863522881f094
bitrat <bitrat.awfulgirl@gmail.com>
2017-10-15 17:10:05 +1300

    Add powershell modules to copy onto server

--- HealthCheck/Copy-FilesToServer.ps1
+++ HealthCheck/Copy-FilesToServer.ps1
@@ -4,10 +4,14 @@
 $server = "\\ABC-TECH-Server\c$\" ## enter current source folder
 $wkstation = "C:\HealthCheck\" ## enter your destination folder 
 $serverModule = "\\ABC-TECH-Server\c$\Program Files\WindowsPowerShell\Modules"
-$wkstationModule = "C:\HealthCheck\ReportHTML"
+$wkstationModule1 = "C:\HealthCheck\ModulesToCopy\ReportHTML"
+$wkstationModule2 = "C:\HealthCheck\ModulesToCopy\ImportExcel"
 
 #Copy HealthCheck folder
 Copy-Item $wkstation -Destination $server -Recurse -Force
 
 #Copy ReportHTML module 
-Copy-Item $wkstationModule -Destination $serverModule -Recurse -Force
+Copy-Item $wkstationModule1 -Destination $serverModule -Recurse -Force
+
+#Copy ImportExcel module 
+Copy-Item $wkstationModule2 -Destination $serverModule -Recurse -Force
