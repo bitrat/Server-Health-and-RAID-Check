f247493e72fd5d82cf2810377863a7565167dea7
bitrat <bitrat.awfulgirl@gmail.com>
2017-10-19 21:59:04 +1300

    Updated Move-Files script

--- HealthCheck/Move-Files.ps1
+++ HealthCheck/Move-Files.ps1
@@ -4,5 +4,20 @@
 $server = "\\ABC-Tech-Company\c$\HealthCheck\" ## enter current source folder
 $wkstation = "C:\HealthCheck\" ## enter your destination folder 
 
-Get-ChildItem -Path "$($server)*.csv" -Recurse | Move-Item -Destination $wkstation
-Get-ChildItem -Path "$($server)*.html" -Recurse | Move-Item -Destination $wkstation
+# Move files from specified folder - if files are older than a day
+# This prevents files that are currently being written to, from being moved
+Get-Childitem -Path "$($server)*.csv" |
+    Where-Object {$_.LastWriteTime -lt (Get-Date).AddDays(-1)} | 
+    Move-Item -destination $wkstation
+
+Get-Childitem -Path "$($server)*.html" |
+    Where-Object {$_.LastWriteTime -lt (Get-Date).AddDays(-1)} | 
+    Move-Item -destination $wkstation
+
+# Move files from specified folder, including today's files
+#Get-ChildItem -Path "$($server)*.csv" | Move-Item -Destination $wkstation
+#Get-ChildItem -Path "$($server)*.html" | Move-Item -Destination $wkstation
+
+# Move files from subfolders as well
+#Get-ChildItem -Path "$($server)*.csv" -Recurse | Move-Item -Destination $wkstation
+#Get-ChildItem -Path "$($server)*.html" -Recurse | Move-Item -Destination $wkstation
