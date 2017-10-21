7e2a9339776e18a89beeb92a6a052bdd58425442
bitrat <bitrat.awfulgirl@gmail.com>
2017-10-22 08:30:17 +1300

    Added calculate folder size script

---
+++ HealthCheck/Calc-FolderSize.ps1
@@ -0,0 +1,12 @@
+﻿#Calculate specific Folder size
+$server = '\\ABC-TechCompany\C$\Photos\'
+
+$FolderList = Get-ChildItem $server
+Foreach($Folder in $FolderList)
+{
+
+    $FolderSize = Get-ChildItem "$server$Folder" -Recurse | Measure-Object -property length -sum
+	
+	$Folder.Name + " - " + ($FolderSize.Sum/1mb) + " MB"
+}
+
